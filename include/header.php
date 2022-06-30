d8f460828e3e085c23c5df5ba9a8e446717d03e8
Aysel Abdullayeva <abdulla2001aysel@gmail.com>
2022-06-30 12:09:26 +0400

    new commit

---
+++ include/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Signy</title>
+
+    <link rel="icon" href="./assets/image/logo.svg" />
+
+    <!-- fontawesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw==" crossorigin="anonymous" referrerpolicy="no-referrer"
+    />
+
+    <!-- bootstrap css file -->
+    <link rel="stylesheet" href="./assets/vendor/bootstrap/css/bootstrap.min.css" />
+
+    <!-- 404 page css -->
+    <link rel="stylesheet" href="./assets/css/404-page.css">
+    <!-- about page css -->
+    <link rel="stylesheet" href="./assets/css/about.css">
+    <!-- question page css -->
+    <link rel="stylesheet" href="./assets/css/questions.css">
+    <!-- video page css  -->
+    <link rel="stylesheet" href="./assets/css/video.css">
+
+    <!-- main css file -->
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <!-- contact css file -->
+    <link rel="stylesheet" href="./assets/css/contact.css" />
+    <!-- blog css file -->
+    <link rel="stylesheet" href="./assets/css/blog.css" />
+    <!-- blog-post css file -->
+    <link rel="stylesheet" href="./assets/css/blog-post.css">
+</head>
